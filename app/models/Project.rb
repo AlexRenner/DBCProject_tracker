b9c1294781a1ff230573783126c23e9c37e21d06
Alex Renner <email": null>
2016-09-14 18:32:36 -0500

    Wrote Project methods for calculating mean and mode of vote values

--- app/models/Project.rb
+++ app/models/Project.rb
@@ -3,4 +3,29 @@ class Project < ActiveRecord::Base
   belongs_to :cohort
   belongs_to :originator, class_name: 'User', foreign_key: :user_id
   has_many :votes
+
+  def mean_vote_value
+    all_votes = self.votes
+
+    avg = (all_votes.map { |vote| vote[:value].to_f }.reduce(:+) / all_votes.length).round(2)
+  end
+
+  def find_vote_values
+    vote_value_array = Array.new
+
+    self.votes.each do |vote|
+      vote_value_array << vote[:value]
+    end
+
+    vote_value_array
+  end
+
+  def vote_frequency
+    find_vote_values.inject(Hash.new(0)) { |hash,value| hash[value] += 1; hash }
+  end
+
+  def mode_vote_value
+    find_vote_values.max_by { |v| vote_frequency[v] }
+  end
+
 end
