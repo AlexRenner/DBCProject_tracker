7fd3c2a79b162795c49ffea5ec7e0f1cee6c4f1e
Alex Renner <email": null>
2016-09-14 16:48:33 -0500

    Corrected team associations

--- app/models/Project.rb
+++ app/models/Project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :team
   belongs_to :cohort
-  belongs_to :originator, class_name: 'User'
+  belongs_to :originator, class_name: 'User', foreign_key: :user_id
   has_many :votes
 end
--- app/models/Vote.rb
+++ app/models/Vote.rb
@@ -1,5 +1,5 @@
 class Vote < ActiveRecord::Base
  belongs_to :round
- belongs_to :voter, class_name: 'User'
+ belongs_to :voter, class_name: 'User', foreign_key: :user_id
  belongs_to :project
 end
