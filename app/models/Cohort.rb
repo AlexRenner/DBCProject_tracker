3cb50f0c386879785348e8ecf3ffeb64916c7e12
Alex Renner <email": null>
2016-09-15 14:49:44 -0500

    Rewrote round/cohort associations

--- app/models/Cohort.rb
+++ app/models/Cohort.rb
@@ -2,5 +2,5 @@ class Cohort < ActiveRecord::Base
   has_many :students, class_name: 'User'
   has_many :projects
   has_many :votes, through: :students
-  has_many :rounds, through: :votes, source: :round
+  has_many :rounds
 end
