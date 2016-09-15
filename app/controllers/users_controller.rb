07165d049a1d7c5ef63bc6b7a0834aa1c6fa7085
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2016-09-14 19:23:30 -0500

    user registration works

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+		# # render plain: params[:article].inspect
+		# p user_params
+	@user = User.new(user_params)#private method call
+	if @user.save
+		session[:user_id] = @user.id
+		p "@@@@@@@@@@@@@@"
+		p session[:user_id]
+		redirect_to @user #redirects to the show action
+	else
+		render 'new'
+	end
+  end
+
+
+  private
+  def user_params
+    p params.require(:user).permit(:first_name, :last_name, :email, :password, :cohort_id)
+  end
+
+end
--- app/models/User.rb
+++ app/models/User.rb
@@ -1,3 +1,25 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
+  validates :email, :presence => true,
+                       :uniqueness => true
+  validates :first_name, :last_name, :password_hash, :presence => true
 
+
+  def password
+	@password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @raw_password = new_password
+  	@password = BCrypt::Password.create(new_password)
+  	self.password_hash = @password
+  end
+
+  def self.authenticate(email, pass)
+  	user_login = User.find_by(email: email) #returns user
+  	if user_login && user_login.password == pass
+  		return user_login
+  	end
+	  nil
+  end
 end
