0842dc52643bb9e6ff4fc05dee3fb764cf4584fe
David Armbrust <david.armbrust@gmail.com>
2016-09-15 15:32:09 -0500

    Add after create to build votes

--- app/models/Round.rb
+++ app/models/Round.rb
@@ -2,4 +2,16 @@ class Round < ActiveRecord::Base
   belongs_to :cohort
   has_many :votes
   has_many :students, through: :votes, class_name: 'User'
+
+  after_save do
+    users = User.all.where(cohort: self.cohort)
+    # TODO add additional filter for project status
+    projects = Project.all.where(cohort: self.cohort)
+
+    users.each do |user|
+      projects.each do |project|
+        self.votes << Vote.create(voter: user, project: project, value: 0)
+      end
+    end
+  end
 end
