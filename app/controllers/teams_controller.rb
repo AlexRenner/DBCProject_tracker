a54c41d22f34a5dbb0f7cbc3cd809d93e3b4e8cb
Alex Renner <email": null>
2016-09-15 21:01:02 -0500

    Added logic for creating a team and associating students and project

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,6 +7,24 @@ class TeamsController < ApplicationController
   end
 
   def create
-
+    @cohort = Cohort.find(params[:cohort_id])
+    @team = Team.new(team_params[:name])
+    if @team.save
+      student_tags = []
+      params.each do |key, value|
+        if key.include?("student")
+          student_tags << key.to_s
+        end
+      end
+      student_ids = student_tags.map{|string| string.delete('student_')}
+      student_ids.each do |id|
+        Student.find(id).team = @team
+      end
+      Project.find(team_params[:project_id]).team = @team
+      render cohort_teams_path(@cohort)
+    else
+      @errors = @team.errors.full_messages
+      render new_cohort_team_path
+    end
   end
 end
