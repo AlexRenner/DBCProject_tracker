0035c13d2293f69086aa2e7a655b1afff44dedde
David Armbrust <david.armbrust@gmail.com>
2016-09-16 15:08:32 -0500

    Update to user login for errors and redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.authenticate(params['email'], params['password'])
-    if user
-      session[:user_id] = user.id
-      redirect_to '/'
+    @user = User.authenticate(params['email'], params['password'])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
-      redirect_to '/'
+      @errors = 'Email or password is incorrect'
+      render 'new'
     end
   end
 
